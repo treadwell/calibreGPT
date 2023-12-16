5d55f7b51cc9d30f3bead99902a905ecead4a13c
treadwell <brooks.ken@gmail.com>
2023-12-16 15:33:57 -0500

    Update test.sh to debug no text error

--- test.sh
+++ test.sh
@@ -3,14 +3,20 @@
 set -e
 
 PROMPT='Cloud Computing in Higher Education'
+
 FP_FULLTEXT_DB='/Users/kbrooks/Dropbox/Books/Calibre Travel Library/full-text-search.db'
 FP_METADATA_DB='/Users/kbrooks/Dropbox/Books/Calibre Travel Library/metadata.db'
 FP_CALIBREGPT_DB='/Users/kbrooks/Dropbox/Books/Calibre Travel Library/calibregpt.db'
 FP_FAISS_INDEX='/Users/kbrooks/Dropbox/Books/Calibre Travel Library/faiss.idx'
 
+# FP_FULLTEXT_DB='/Users/kbrooks/Dropbox/Books/calbreGPT/full-text-search.db'
+# FP_METADATA_DB='/Users/kbrooks/Dropbox/Books/calbreGPT/metadata.db'
+# FP_CALIBREGPT_DB='/Users/kbrooks/Dropbox/Books/calbreGPT/calibregpt.db'
+# FP_FAISS_INDEX='/Users/kbrooks/Dropbox/Books/calbreGPT/faiss.idx'
+
 clear
 
-# export DEBUG=1
+export DEBUG=1
 
 echo "testing --prompt"
 python3 engine.py \
@@ -30,4 +36,4 @@ python3 engine.py \
     --calibregpt-db "$FP_CALIBREGPT_DB" \
     --faiss-index "$FP_FAISS_INDEX" \
     --match-count 10 \
-    --ids "13880"
+    --ids "12653"
