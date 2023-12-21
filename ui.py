b16e0c88afbe2619d6280c5388a1aaf975a82a7b
treadwell <brooks.ken@gmail.com>
2023-12-21 10:44:01 -0500

    WIP Adding second icon to context menu

--- ui.py
+++ ui.py
@@ -1,35 +1,43 @@
 from calibre.gui2.actions import InterfaceAction
 from calibre_plugins.calibre_gpt.main import GPTDialog
+from PyQt5.QtWidgets import QAction, QIcon
 import subprocess
+
+def show_dialog(gui, qaction, do_user_config, type):
+    # base_plugin_object = self.interface_action_base_plugin
+    # do_user_config = base_plugin_object.do_user_config
+    # d = GPTDialog(self.gui, self.qaction.icon(), do_user_config, type)
+    d = GPTDialog(gui, qaction.icon(), do_user_config, type)
+    d.show()
 
 class InterfacePlugin(InterfaceAction):
 
-    name = 'Calibre GPT'
+    name = 'Calibre GPT Query Books'
     action_spec = ('Calibre GPT', None, 'Run the Calibre GPT Plugin', None)
-    action_menu_clone_qaction = "Calibre GPT Similar Books"
+
+    def initialize(self):
+        self.gui.add_iaction(InterfacePluginSecondary(self.gui, None))
+        # Need properties to make it popup...try another function
+        # something other than genesis().
 
     def genesis(self):
-        text = "Calibre GPT Similar Books"
-        from PyQt5.QtWidgets import QAction, QIcon
-        icon = get_icons('images/icon.png', 'Calibre GPT Plugin')
-        self.qaction.setIcon(icon)
-        self.qaction.triggered.connect(self.show_dialog)
-        ma = QAction(QIcon.ic(icon), text, self.gui)
-        ma.setAutoRepeat(self.auto_repeat)
-        ma.setToolTip(text)
-        ma.setStatusTip(text)
-        ma.setWhatsThis(text)
-        self.gui.addAction(ma)
-        ma.triggered.connect(self.show_dialog)
-        
-        # TODO: double check that we need this
         subprocess.run(["pip3", "install", "certifi", "faiss-cpu", "numpy"])
+        self.icon = get_icons('images/icon.png', self.name)
+        self.qaction.setIcon(self.icon)
+        self.qaction.triggered.connect(self.show)
 
-    def show_dialog(self):
-        # from PyQt5.QtWidgets import QToolButton
-        base_plugin_object = self.interface_action_base_plugin
-        do_user_config = base_plugin_object.do_user_config
-        d = GPTDialog(self.gui, self.qaction.icon(), do_user_config)
-        # print(self.qaction.text(), self.qaction.menu())
-        # print([(o, o.parent(), o.sender()) for o in self.qaction.associatedObjects() if type(o) is QToolButton])
-        d.show()
+    def show(self):
+        show_dialog(self.gui, self.qaction, self.interface_action_base_plugin.do_user_config, "main")
+
+class InterfacePluginSecondary(InterfaceAction):
+
+    name = 'Calibre GPT Similar Books'
+    action_spec = ('Calibre GPT', None, 'Run the Calibre GPT Plugin', None)
+
+    def genesis(self):
+        self.icon = get_icons('images/icon.png', self.name)
+        self.qaction.setIcon(self.icon)
+        self.qaction.triggered.connect(self.show)
+
+    def show(self):
+        show_dialog(self.gui, self.qaction, self.interface_action_base_plugin.do_user_config, "context")
